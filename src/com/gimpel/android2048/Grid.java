eee3622fa4c09cf600762d5c2e0089324b0e6144
filip gimpel <filip.gimpel@gmail.com>
2014-04-29 18:46:12 +0200

    fix 0224 -> 8 bug, add isGameOver method

--- src/com/gimpel/android2048/Grid.java
+++ src/com/gimpel/android2048/Grid.java
@@ -44,6 +44,31 @@ public class Grid {
 	}
 	
 	/**
+	 * Checks if we can make any more moves, or if the game is over
+	 */
+	public boolean isGameLost() {
+		// if there are still some empty spots on the grid, game is surely not over
+		if (getEmptyElements().size() != 0) return false;
+		
+		// checking whether there are two elements in grid
+		// next to each other horizontally or vertically
+		// if so, return false, cause we can still play
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (j!=3) { /*check right */
+					if (mArray[i][j] == mArray[i][j+1]) return false; 
+				}
+				if (i!=3) { /*check bottom*/
+					if (mArray[i][j] == mArray[i+1][j]) return false;
+				}
+			}
+		}
+		
+		// otherwise, game over!
+		return true;
+	}
+	
+	/**
 	 * Adds random element to the grid if there is an empty slot available
 	 *  
 	 * @return Position of random element added to the grid in empty slot, 
@@ -59,7 +84,7 @@ public class Grid {
 			Point randomPoint = emptyElements.get(randomPosition);
 			mArray[randomPoint.x][randomPoint.y] = 2;
 			result = randomPoint.x*4 + randomPoint.y;
-		} else { // Game over!
+		} else {
 			result = -1;
 		}
 		
@@ -121,6 +146,8 @@ public class Grid {
 					mArray[h][i] = 0;
 					
 					mScore += mArray[h][i-1]; 
+					
+					i++;
 				}
 			}
 		}
@@ -159,6 +186,8 @@ public class Grid {
 					mArray[h][i] = 0;
 					
 					mScore += mArray[h][i-1];
+					
+					i--;
 				}
 			}
 		}
@@ -197,6 +226,8 @@ public class Grid {
 					mArray[i][h] = 0;
 					
 					mScore += mArray[i-1][h]; 
+					
+					i--;
 				}
 			}
 		}
@@ -237,6 +268,8 @@ public class Grid {
 					mArray[i][h] = 0;
 					
 					mScore += mArray[i-1][h];
+					
+					i++;
 				}
 			}
 		}
